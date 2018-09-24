c7f438d03576027fc8b0ed214369dba58f353180
forgo <elliott.richerson@gmail.com>
2018-09-24 16:31:20 -0600

    reintroduce afterEvaluate

--- src/main/java/io/forgo/plugins/gradle/KeystorePlugin.java
+++ src/main/java/io/forgo/plugins/gradle/KeystorePlugin.java
@@ -20,40 +20,48 @@ public class KeystorePlugin implements Plugin<Project> {
 
     private void addSSLKeyTask(Project project, KeystoreExtension extension) {
         project.getTasks().create(TASK_SSL_KEY, SSLKeyTask.class, task -> {
-            task.setKeyFile(extension.getKeyFile());
-            task.setKeyPassword(extension.getKeyPassword());
+            project.afterEvaluate(p -> {
+                task.setKeyFile(extension.getKeyFile());
+                task.setKeyPassword(extension.getKeyPassword());
+            });
         });
     }
 
     private void addSSLCertTask(Project project, KeystoreExtension extension) {
         project.getTasks().create(TASK_SSL_CERT, SSLCertTask.class, task -> {
-            task.setKeyFile(extension.getKeyFile());
-            task.setKeyPassword(extension.getKeyPassword());
-            task.setCertFile(extension.getCertFile());
+            project.afterEvaluate(p -> {
+                task.setKeyFile(extension.getKeyFile());
+                task.setKeyPassword(extension.getKeyPassword());
+                task.setCertFile(extension.getCertFile());
+            });
             task.dependsOn(project.getTasks().getByName(TASK_SSL_KEY));
         });
     }
 
     private void addPKCS12Task(Project project, KeystoreExtension extension) {
         project.getTasks().create(TASK_PKCS12, PKCS12Task.class, task -> {
-            task.setKeyFile(extension.getKeyFile());
-            task.setKeyPassword(extension.getKeyPassword());
-            task.setCertFile(extension.getCertFile());
-            task.setPkcs12File(extension.getPkcs12File());
-            task.setPkcs12Password(extension.getPkcs12Password());
-            task.setKeystoreAlias(extension.getAlias());
+            project.afterEvaluate(p -> {
+                task.setKeyFile(extension.getKeyFile());
+                task.setKeyPassword(extension.getKeyPassword());
+                task.setCertFile(extension.getCertFile());
+                task.setPkcs12File(extension.getPkcs12File());
+                task.setPkcs12Password(extension.getPkcs12Password());
+                task.setKeystoreAlias(extension.getAlias());
+            });
             task.dependsOn(project.getTasks().getByName(TASK_SSL_CERT));
         });
     }
 
     private void addJKSTask(Project project, KeystoreExtension extension) {
         project.getTasks().create(TASK_JKS, JKSTask.class, task -> {
-            task.setKeyFile(extension.getKeyFile());
-            task.setCertFile(extension.getCertFile());
-            task.setPkcs12File(extension.getPkcs12File());
-            task.setPkcs12Password(extension.getPkcs12Password());
-            task.setKeystoreFile(extension.getJksFile());
-            task.setKeystorePassword(extension.getJksPassword());
+            project.afterEvaluate(p -> {
+                task.setKeyFile(extension.getKeyFile());
+                task.setCertFile(extension.getCertFile());
+                task.setPkcs12File(extension.getPkcs12File());
+                task.setPkcs12Password(extension.getPkcs12Password());
+                task.setKeystoreFile(extension.getJksFile());
+                task.setKeystorePassword(extension.getJksPassword());
+            });
             task.dependsOn(project.getTasks().getByName(TASK_PKCS12));
         });
     }
