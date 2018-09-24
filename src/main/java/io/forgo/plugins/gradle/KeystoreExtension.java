3edc0cfc20414cb25fdda877fc51418fb3799f46
forgo <elliott.richerson@gmail.com>
2018-09-23 21:05:53 -0600

    extension updates

--- src/main/java/io/forgo/plugins/gradle/KeystoreExtension.java
+++ src/main/java/io/forgo/plugins/gradle/KeystoreExtension.java
@@ -1,33 +1,17 @@
 package io.forgo.plugins.gradle;
 
 import org.gradle.api.Project;
-import org.gradle.api.tasks.Input;
 
 public class KeystoreExtension {
 
-    @Input
-    private String keyFile;
-
-    @Input
-    private String keyPassword;
-
-    @Input
-    private String certFile;
-
-    @Input
-    private String pkcs12File;
-
-    @Input
-    private String pkcs12Password;
-
-    @Input
-    private String keystoreFile;
-
-    @Input
-    private String keystorePassword;
-
-    @Input
-    private String keystoreAlias;
+    private final String keyFile;
+    private final String keyPassword;
+    private final String certFile;
+    private final String pkcs12File;
+    private final String pkcs12Password;
+    private final String keystoreFile;
+    private final String keystorePassword;
+    private final String keystoreAlias;
 
     public KeystoreExtension(Project project) {
         // set default values here
@@ -36,9 +20,8 @@ public class KeystoreExtension {
         this.certFile = "debug.crt";
         this.pkcs12File = "debug.pkcs12";
         this.pkcs12Password = "password";
-        this.keystoreFile = "keystore";
-        this.keystorePassword = "password";
         this.keystoreFile = "keystore.jks";
+        this.keystorePassword = "password";
         this.keystoreAlias = "jetty";
     }
 
@@ -46,63 +29,31 @@ public class KeystoreExtension {
         return keyFile;
     }
 
-    public void setKeyFile(String keyFile) {
-        this.keyFile = keyFile;
-    }
-
     public String getKeyPassword() {
         return keyPassword;
-    }
-
-    public void setKeyPassword(String keyPassword) {
-        this.keyPassword = keyPassword;
     }
 
     public String getCertFile() {
         return certFile;
     }
 
-    public void setCertFile(String certFile) {
-        this.certFile = certFile;
-    }
-
     public String getPkcs12File() {
         return pkcs12File;
-    }
-
-    public void setPkcs12File(String pkcs12File) {
-        this.pkcs12File = pkcs12File;
     }
 
     public String getPkcs12Password() {
         return pkcs12Password;
     }
 
-    public void setPkcs12Password(String pkcs12Password) {
-        this.pkcs12Password = pkcs12Password;
-    }
-
     public String getKeystoreFile() {
         return keystoreFile;
-    }
-
-    public void setKeystoreFile(String keystoreFile) {
-        this.keystoreFile = keystoreFile;
     }
 
     public String getKeystorePassword() {
         return keystorePassword;
     }
 
-    public void setKeystorePassword(String keystorePassword) {
-        this.keystorePassword = keystorePassword;
-    }
-
     public String getKeystoreAlias() {
         return keystoreAlias;
-    }
-
-    public void setKeystoreAlias(String keystoreAlias) {
-        this.keystoreAlias = keystoreAlias;
     }
 }
